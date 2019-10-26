8f63bbe73657f22e59b9e21dcac22ee72db501dc
Nikita Nafranets <eddimensi@gmail.com>
2019-10-26 19:19:49 +0300

    add autonext slide

--- src/features/Player/components/ControlContainer.js
+++ src/features/Player/components/ControlContainer.js
@@ -29,6 +29,7 @@ const Container = styled.div`
   overflow: hidden;
   display: flex;
   align-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
 `
--- src/features/Player/components/VideoWidget.js
+++ src/features/Player/components/VideoWidget.js
@@ -2,7 +2,7 @@ import React, { useRef, useLayoutEffect } from 'react'
 import styled from '@emotion/styled'
 import { useStore } from 'effector-react'
 
-import { $playing, $muted } from 'src/features/Player/store/player'
+import { $playing, $muted, $currentSlide, forwarded } from 'src/features/Player/store/player'
 
 const Video = styled.video`
   position: absolute;
@@ -13,6 +13,7 @@ const Video = styled.video`
 export function VideoWidget({ Source: { Src, ContentType }, WidgetID, className }) {
   const playing = useStore($playing)
   const isMuted = useStore($muted)
+  const slide = useStore($currentSlide)
   const ref = useRef(null)
   const promise = useRef(null)
   useLayoutEffect(() => {
@@ -36,7 +37,14 @@ export function VideoWidget({ Source: { Src, ContentType }, WidgetID, className 
     ref.current.muted = isMuted
   }, [playing, isMuted])
   return (
-    <Video controls={false} key={WidgetID} className={className} ref={ref} playsInline>
+    <Video
+      controls={false}
+      key={WidgetID}
+      className={className}
+      ref={ref}
+      playsInline
+      onEnded={() => (slide.NextSlideID ? forwarded(slide.NextSlideID) : undefined)}
+    >
       <source src={Src} type={ContentType} />
     </Video>
   )
